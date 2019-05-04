8dace62f6e69315542e4bdc31ffe40cbc579f8ef
Bill Chen <Bill.Chen@live.com>
2019-05-05 01:43:27 +0800

    更新 Readme，同时完善程序

--- crawller.py
+++ crawller.py
@@ -1,6 +1,6 @@
-##############################################
-# 依赖库 pycrypto ，request，pillow，pyexecjs  #
-##############################################
+##########################################
+# 依赖库 lxml ，request，pillow，pyexecjs  #
+##########################################
 
 import requests
 import lxml
@@ -15,15 +15,17 @@ from lxml import etree
 
 mainurl = 'https://portal1.ecnu.edu.cn/cas/login?service=http%3A%2F%2Fapplicationnewjw.ecnu.edu.cn%2Feams%2Fhome.action'
 tabelurl = 'http://applicationnewjw.ecnu.edu.cn/eams/courseTableForStd!courseTable.action'
+idsurl = 'http://applicationnewjw.ecnu.edu.cn/eams/courseTableForStd!index.action'
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
 	'Refer': 'https://portal1.ecnu.edu.cn/cas/login?service=http%3A%2F%2Fapplicationnewjw.ecnu.edu.cn%2Feams%2Fhome.action%3Bjs'}
 captachaurl = 'https://portal1.ecnu.edu.cn/cas/code'
 s = requests.session()
-
+ids = 0
 	
 
 def GetCode():
+	"发送一次新的 get 请求并获取验证码，让用户填写。"
 	r = s.get(mainurl, headers=headers)
 	imgraw = s.get(captachaurl)
 	with open(sys.path[0] + '/data/temp.jpg', 'wb+') as f:
@@ -73,6 +75,7 @@ def LoginLooper(username='', password='', ifEnterPassword=False):
 	if len(errors) == 0:
 		realName = elements.xpath('//a[contains(@title, "查看登录记录")]/font/text()')[0]
 		print('登陆成功:', realName)
+		defineIDS()
 		return 0
 	elif elements.xpath('//*[@id="errormsg"]/text()')[0] == "验证码有误":
 		return 1
@@ -81,21 +84,33 @@ def LoginLooper(username='', password='', ifEnterPassword=False):
 	else:
 		return 3
 
+def defineIDS():
+	"获取用户标识 ids"
+	global ids
+	r = s.get(idsurl)
+	src = r.text
+	rule = re.compile('bg\.form\.addInput\(form,"ids","(.*?)"\);', re.S)
+	ids = re.findall(rule, src)[0]
+	pass
+	
+
+	
+
 def TableSolver():
 	tablePostData = {'ignoreHead': 1,
                   'setting.kind': 'std',
                   'startWeek': 1,
                   'semester.id': 737,
-                  'ids': 567403}
+                  'ids': ids}
 	# semester.id：2018-2019学年度上学期为737，每向前/向后一个学期就增加/减少32.
 	semesterIDs = [705, 737, 769]
 	r = s.post(tabelurl, data=tablePostData)
 	elements = etree.HTML(r.content)
 	# 提取出具有课程的数据
 	raws = elements.xpath('//*[@language="JavaScript"][3]/text()')[0]
+	print(raws)
 	rule = re.compile('new TaskActivity\((.*?)\);', re.S)
 	classes = re.findall(rule, raws)
-	print(classes)
 	processedClass = []
 	for one in classes:
 		temp = one.split(',')
@@ -107,12 +122,15 @@ def TableSolver():
 
 def DumpJson():
 	pass
+
 def main():
 	print('这个脚本可以直接登陆网站，爬取课程表，并生成一个 xjd 标准的 json 课程表文件。')
 	print('另外由于 Policy 原因，你可能时常需要准备好学校的 VPN.')
 	print('正在初始化...')
-	username = input('请输入你的公共数据用户名（学号）：')
-	password = getpass.getpass('请输入你的公共数据库密码（直接输入即可，已关闭输入回显）：')
+	# username = input('请输入你的公共数据用户名（学号）：')
+	# password = getpass.getpass('请输入你的公共数据库密码（直接输入即可，已关闭输入回显）：')
+	username = '10185101210'
+	password = 'Gerenyinsibc36'
 	feedback = LoginLooper(username, password, ifEnterPassword=False)
 	while feedback != 0:
 		if feedback == 1:
--- readme.md
+++ readme.md
@@ -1,2 +1,36 @@
-Want to know how to make the timetable?
+# ECNU-class2ics
+
+## What's this
+
+ics 是国际通用的最为流行的日历文件之一，受到几乎所有日历软件的支持。对于华东师范大学的学生，这个程序会登陆华东师范大学公共数据库，获取你的课程表，并根据指定的日期和提醒策略，生成一个可以导入到各个日历软件中的 ics 文件。对于其他高校的学生，可以手动输入信息。
+
+## Why we made this
+
+- 使用日历软件管理课程表可以让你不再把丑陋的课程表设置成桌面壁纸。
+- 每节课上课前都会收到关于下一节课的提醒。（美观程度取决于系统和日历软件）
+- 对于 iOS 和 macOS，系统会自动为你查找上课地点并接入苹果地图。
+- 不想每学期都手动添加。~~（懒）~~
+  
+## How to use this
+
+需 Python 3 环境，依赖以下运行库：
+
+**lxml, request, pillow, pyexecjs**
+
+如果运行的时候还缺了什么，请 pip 上。
+
+对于华东师范大学的学生，直接运行 crawller.py 和 main.py 即可。对于其他高校的学生，请先在`conf_classTime.json`中配置每节课的时间，然后手动填写`classinfo.xlsx`。具体操作可以参照文末的[官方文档]("http://xiejiadong.com/?p=419")。
+
+最后生成的文件为同目录下的 class.ics.
+
+## About this
+
+Contributor|Website
+---|---
+Bill Chen|https://billc.io
+Xiejiadong|http://xiejiadong.com/
+
+> Old Doc:
+
+Want to know how to make the timetable? 
 http://xiejiadong.com/?p=419
