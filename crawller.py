56fc7dd84b50ec17709b6a2a0aaf3b7ba8c41760
Bill Chen <Bill.Chen@live.com>
2019-05-05 20:03:41 +0800

    crawller.py 功能完成
    可以从 crawller.py 直接获取 ECNU 公共数据库的课程内容。semester.id 通过远程服务器控制。

--- crawller.py
+++ crawller.py
@@ -10,7 +10,7 @@ import json
 import sys
 import getpass
 import execjs
-from PIL import Image
+import platform
 from lxml import etree
 
 mainurl = 'https://portal1.ecnu.edu.cn/cas/login?service=http%3A%2F%2Fapplicationnewjw.ecnu.edu.cn%2Feams%2Fhome.action'
@@ -30,10 +30,25 @@ def GetCode():
 	imgraw = s.get(captachaurl)
 	with open(sys.path[0] + '/data/temp.jpg', 'wb+') as f:
 		f.write(imgraw.content)
-	img = Image.open(sys.path[0] + '/data/temp.jpg')
-	img.show()
+	OpenFile(sys.path[0] + '/data/temp.jpg')
 	captacha = input('请输入验证码：')
 	return captacha
+
+def OpenFile(filedir):
+	if (platform.system() == 'Windows'):
+		import os
+		os.startfile(filedir)
+	elif (platform.system() == 'Darwin'):
+		import subprocess
+		subprocess.call(["open", filedir])
+	elif (platform.system() == 'Linux'):
+		import subprocess
+		subprocess.call(["xdg-open", filedir])
+	else:
+		from PIL import Image
+		img = Image.open(filedir)
+		img.show()
+
 
 def GetRSA(username, password):
 	# 获取 des.js 里的内容
@@ -59,7 +74,7 @@ def LoginLooper(username='', password='', ifEnterPassword=False):
 	if (ifEnterPassword == True):
 		username = input('请输入你的公共数据用户名（学号）：')
 		password = getpass.getpass('请输入你的公共数据库密码（直接输入即可，已关闭输入回显）：')
-	code=GetCode()
+	code = GetCode()
 	postData = {
 		'code': code,
 		'rsa': GetRSA(username, password),
@@ -75,7 +90,6 @@ def LoginLooper(username='', password='', ifEnterPassword=False):
 	if len(errors) == 0:
 		realName = elements.xpath('//a[contains(@title, "查看登录记录")]/font/text()')[0]
 		print('登陆成功:', realName)
-		defineIDS()
 		return 0
 	elif elements.xpath('//*[@id="errormsg"]/text()')[0] == "验证码有误":
 		return 1
@@ -84,53 +98,167 @@ def LoginLooper(username='', password='', ifEnterPassword=False):
 	else:
 		return 3
 
-def defineIDS():
-	"获取用户标识 ids"
+def DefineIDS():
+	"获取用户标识 ids，改变全局变量"
 	global ids
 	r = s.get(idsurl)
 	src = r.text
 	rule = re.compile('bg\.form\.addInput\(form,"ids","(.*?)"\);', re.S)
 	ids = re.findall(rule, src)[0]
-	pass
+	print('ids:',ids)
+	return ids
 	
-
-	
+def GetSemesterID():
+	print('正在获取 semester.id...')
+	web = requests.get('https://billc.io/conf-ecnu-class2ics/')
+	elements = etree.HTML(web.content)
+	id = elements.xpath('//strong/text()')[0]
+	print('semester.id:', id)
+	return id
 
 def TableSolver():
 	tablePostData = {'ignoreHead': 1,
                   'setting.kind': 'std',
                   'startWeek': 1,
-                  'semester.id': 737,
+                  'semester.id': GetSemesterID(),
                   'ids': ids}
-	# semester.id：2018-2019学年度上学期为737，每向前/向后一个学期就增加/减少32.
+	# semester.id: 2018-2019学年度上学期为737，每向前/向后一个学期就增加/减少32.
 	semesterIDs = [705, 737, 769]
 	r = s.post(tabelurl, data=tablePostData)
 	elements = etree.HTML(r.content)
 	# 提取出具有课程的数据
 	raws = elements.xpath('//*[@language="JavaScript"][3]/text()')[0]
-	print(raws)
-	rule = re.compile('new TaskActivity\((.*?)\);', re.S)
-	classes = re.findall(rule, raws)
-	processedClass = []
-	for one in classes:
-		temp = one.split(',')
-		for i in range(len(temp)):
-			temp[i] = temp[i].replace('"', '')
-		processedClass.append(temp)
-	
+	# print(raws)
+	# 使用新的正则表达式匹配每一节课所在的时间
+	rawClasses = re.findall('(.\*unitCount\+\d+)|new TaskActivity\((.*?),null', raws)
+	processing = []
+	processed = []
+	i = 0
+	dynaLen = len(rawClasses)
+	# 第一次处理混乱的数据
+	# processing 数据结构:
+	# [
+	# 	[杂糅在一起的课程名教师地点和上课周等等, [每周上课的星期, [每天要上的节数 n1, n2, ...]]],
+	# 	[第二门课],
+	# 	[第三门课]...
+	# ]
+	while i < dynaLen - 2:
+		temp = []
+		if (len(rawClasses[i][0]) == 0):
+			temp.append(rawClasses[i][1])
+			whichDay = 0
+			allUnits = []
+			while (len(rawClasses[i + 1][0]) != 0):
+				whichDay = int(rawClasses[i + 1][0][0])
+				unit = rawClasses[i + 1][0][12:]
+				allUnits.append(unit)
+				# 将截取出来的 str 类型转换成 int 方便计算
+				allUnits = list(map(int, allUnits))
+				if (i == len(rawClasses) - 2):
+					break
+				del rawClasses[i + 1]
+			temp.append([whichDay, allUnits])
+			processing.append(temp)
+		i = i + 1
+		dynaLen = len(rawClasses)
+	# 第二次整理数据
+	# processed 数据结构：
+	# [
+	# 	[教师名字，课程名，上课地点，起始周数，结束周数，单双周类型:1单周2双周3每周，星期, [每天要上的节数 n1, n2, ...],
+	# 	[第二门课],
+	# 	[第三门课]...
+	# ]
+	print('已获取课程:')
+	for one in processing:
+		temp = []
+		temp.extend(one[0].replace('"', '').split(','))
+		del temp[4], temp[2], temp[0]
+		weekData = WeekProcessor(temp[3])
+		del temp[3]
+		temp.extend(weekData)
+		temp.extend(one[1])
+		processed.append(temp)
+		print(temp[1])
+	return processed
+
+def WeekProcessor(raw):
+	"""
+	输入：类似于 0111111111111111111000000000000000000000 的周数信息
+	返回值：一个列表[起始周数, 结束周数, 1单周2双周3每周]
+	"""
+	weekData = []
+	begin = 0
+	end = 30
+	typeFlag = 0
+	beginFixed = False
+	endFixed = False
+	for i in range(len(raw)):
+		if (raw[i] == '1' and beginFixed == False):
+			beginFixed = True
+			begin = i
+			if (i % 2 == 1 and raw[i + 1] == '0'):
+				typeFlag = 1
+			elif (i % 2 == 0 and raw[i + 1] == '0'):
+				typeFlag = 2
+			elif (raw[i + 1] == '1' and raw[i+2] == '1'):
+				typeFlag = 3
+			else:
+				typeFlag = 3
+		if (raw[i] == '0' and raw[i + 1] == '0' and beginFixed == True and endFixed == False):
+			endFixed = True
+			end = i - 1
+		if (beginFixed == True and endFixed == True):
+			break
+	return [begin, end, typeFlag]
 	pass
 
-def DumpJson():
+	return weekData
+
+
+
+def DumpJson(classList):
+	classInfo = {'classInfo': []}
+	with open(sys.path[0] + '/conf_classTime.json', 'r') as f:
+		classTime = json.load(f)
+	classTimes = []
+	# 获取 conf_classTime.json 中的配置文件，并和 classList 的配置匹配
+	# 由于 classList 中的课程节数从 0 开始，所以需要从 json 中 - 1 匹配
+	for oneTime in classTime['classTime']:
+		temp = re.findall(r"\d+\.?\d*",oneTime['name'])
+		temp = list(map(int, temp))
+		for i in range(len(temp)):
+			temp[i] = temp[i] - 1
+		classTimes.append(temp)
+	
+	for aClass in classList:
+		nameRaw = aClass[1]
+		nameNew = re.sub('\([\.A-Z0-9]*?\)', '', nameRaw)
+		classData = {'className': nameNew,
+                    'week': {'startWeek': aClass[3], 'endWeek': aClass[4]},
+                    'weekday': aClass[6] + 1,
+                    'weeks': aClass[5],
+                    'classTime': classTimes.index(aClass[7]) + 1,
+                    'classroom': '教书院128'}
+		classInfo['classInfo'].append(classData)
+	with open(sys.path[0] + '/conf_classInfo.json', 'w+', encoding='utf8') as f:
+		json.dump(classInfo, f, indent=4, ensure_ascii=False)
 	pass
+
+def Instruct():
+	print("""
+Welcome!
+-----------------------------------------------------
+该脚本可以登陆华东师范大学公共数据库，并根据课表数据生成
+xjd1.0 标准的 json 课程表文件。
+另外由于 Policy 原因，你可能时常需要准备好学校的 VPN.
+-----------------------------------------------------
+正在初始化...
+""")
 
 def main():
-	print('这个脚本可以直接登陆网站，爬取课程表，并生成一个 xjd 标准的 json 课程表文件。')
-	print('另外由于 Policy 原因，你可能时常需要准备好学校的 VPN.')
-	print('正在初始化...')
-	# username = input('请输入你的公共数据用户名（学号）：')
-	# password = getpass.getpass('请输入你的公共数据库密码（直接输入即可，已关闭输入回显）：')
-	username = '10185101210'
-	password = 'Gerenyinsibc36'
+	Instruct()
+	username = input('请输入你的公共数据用户名（学号）：')
+	password = getpass.getpass('请输入你的公共数据库密码（直接输入即可，已关闭输入回显）：')
 	feedback = LoginLooper(username, password, ifEnterPassword=False)
 	while feedback != 0:
 		if feedback == 1:
@@ -146,7 +274,15 @@ def main():
 				feedback = LoginLooper(ifEnterPassword=True)
 			else:
 				sys.exit()
-	TableSolver()
+
+	print('正在获取用户 ids...')
+	DefineIDS()
+	print('正在公共数据库上获取课程数据...')
+	classList = TableSolver()
+	print('正在处理为 xjd 标准 json 文件...')
+	DumpJson(classList)
+	print('处理完成，课表信息已保存至 conf_classInfo.json 中。')
+
 
 if __name__ == '__main__':
 	main()
